1daf381ea90424cf765d3528105c12f286b51ea0
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-04-26 00:11:58 -0500

    [ADD] Se crea enumeracion de tipo de equipamiento

---
+++ backend/app/models/equipment_type.py
@@ -0,0 +1,11 @@
+import string
+from enum import Enum
+
+class EquipmentTypes(str, Enum):
+
+    CARDIOVASCULAR_MACHINE = "cardiovascular machine"
+    STRENGTH_MACHINE = "strength machine"
+    DUMBBELLS = "dumbbells"
+    BENCH = "bench"
+    ELASTIC_BAND ="elastic band"
+    OTHER = "other"
