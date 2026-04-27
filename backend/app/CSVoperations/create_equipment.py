7f104382fffb489e7c1631233860f9d07f241917
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-04-27 11:36:21 -0500

    [MOD] Se modifica la forma de crear un equipment, se agrega asignacion de id automatico de forma diferente

--- backend/app/CSVoperations/create_equipment.py
+++ backend/app/CSVoperations/create_equipment.py
@@ -1,5 +1,6 @@
 import csv
-from models.equipment import EquipmentId
+import pandas as pd
+from models.equipment import EquipmentId, Equipment
 
 def create_equipment(equipment: list[EquipmentId]):
 
@@ -10,3 +11,21 @@ def create_equipment(equipment: list[EquipmentId]):
 
         for i in equipment:
             writer.writerow(i.model_dump())
+
+def new_id_company():
+    dataFrame = pd.read_csv('equipment.csv')
+    max_id = dataFrame['id_equipment'].max()
+
+    if pd.isna(max_id):
+        return 1
+
+    return max_id+1
+
+def create_equipment_id(equipment: Equipment):
+    id = new_id_company()
+    newEquipment = EquipmentId(**equipment.model_dump(),id_equipment=id)
+    with open('equipment.csv', 'a', newline='') as file:
+        columnas = newEquipment.model_dump().keys()
+        writer = csv.DictWriter(file,fieldnames=columnas)
+        writer.writerow(newEquipment.model_dump())
+    return  newEquipment
