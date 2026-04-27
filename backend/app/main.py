8b00d74706d2cd93cbe1b5a7fa980500aa58ed09
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-04-27 11:44:40 -0500

    [ADD] Se agregan respectivos endpoints para company y se modifican algunas ya existentes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,42 @@
 from fastapi import FastAPI
+from models.equipment import EquipmentId,Equipment
 from models.gimnasio import GimnasioEntrada, GimnasioSalida
-from middlewares.ids_middlewares import put_id_gym_equipment
+from models.company import CompanySalida, CompanyEntrada
 from CSVoperations.create_gym import create_gym
-from CSVoperations.create_equipment import create_equipment
+from CSVoperations.create_equipment import create_equipment_id
 from CSVoperations.create_all_models import create_all_models
+from CSVoperations.find_equipment import find_quipment_id_gym
+from CSVoperations.find_gym import find_gym_id
+from CSVoperations.create_company import new_company_salida
+from CSVoperations.find_company import find_company_id
 
 app = FastAPI(lifespan=create_all_models)
-
-databse = []
 @app.get("/")
 async def root():
     return {"Hello": "Welcome to CSV prototype of gym app"}
 
-@app.get("/gimnasio")
-async def gimnasio_get():
-    return databse
-@app.post("/gimnasio",response_model=GimnasioSalida)
+@app.get(f'/company/{id}')
+async def get_company(id: int):
+    return find_company_id(id)
+@app.get(f"/gimnasio/{id}")
+async def gimnasio_get(id: int):
+    return find_gym_id(id)
+
+@app.get(f"/equipment_of_gym/{id}")
+async def get_equipment_of_gym(id: int):
+    return find_quipment_id_gym(id)
+
+@app.post('/company', response_model=CompanySalida)
+async def company_post(company: CompanyEntrada):
+    return new_company_salida(company)
+@app.post("/gimnasio")
 async def gimnasio_post(gimnasio: GimnasioEntrada):
-    gimnasioSalida = put_id_gym_equipment(gimnasio)
-    create_gym(gimnasioSalida)
-    create_equipment(gimnasioSalida.equipment)
-    return gimnasioSalida
+    if "Error" in find_company_id(gimnasio.id_company):
+        return {"Error": "Company no existe"}
+    return create_gym(gimnasio)
+
+@app.post("/equipment")
+async def equipment_post(equipment: Equipment):
+    if "Error" in find_gym_id(equipment.id_gym):
+        return {"Error": "Gym no existe"}
+    return create_equipment_id(equipment)
