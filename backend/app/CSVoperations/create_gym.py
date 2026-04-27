578bc83dc7cbfd129f368266e3ebdcfdf41b9981
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-04-27 11:36:39 -0500

    [MOD] Se modifica la forma de crear gimnasio y asignar id automaticamente

--- backend/app/CSVoperations/create_gym.py
+++ backend/app/CSVoperations/create_gym.py
@@ -1,9 +1,26 @@
 import csv
-from models.gimnasio import GimnasioSalida
+from models.gimnasio import GimnasioEntrada, GimnasioSalida
+import pandas as pd
 
-def create_gym(gimnasio: GimnasioSalida):
+def new_gym_id() -> int:
+
+    dataFrame = pd.read_csv('gimnasios.csv')
+    max_id = dataFrame['id_gimnasio'].max()
+
+    if pd.isna(max_id):
+        return 1
+
+    return max_id+1
+
+def create_gym(gimnasio: GimnasioEntrada):
+
+    new_id = new_gym_id()
+    print(new_id)
+    gimnasioSalida = GimnasioSalida(id_gimnasio=new_id,**gimnasio.model_dump())
 
     with open("gimnasios.csv",'a',newline='') as file:
-        columnas = gimnasio.model_dump().keys()
+        columnas = gimnasioSalida.model_dump().keys()
         writer = csv.DictWriter(file, fieldnames=columnas)
-        writer.writerow(gimnasio.model_dump())
+        writer.writerow(gimnasioSalida.model_dump())
+
+    return gimnasioSalida
