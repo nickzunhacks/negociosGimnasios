085783ed4ddc18c366660b19a9debdc5902e8656
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-04-27 21:00:15 -0500

    [ADD] se agrega endpoints delete y obtener gimnasios de una company

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,11 +5,13 @@ from models.company import CompanySalida, CompanyEntrada
 from CSVoperations.create_gym import create_gym
 from CSVoperations.create_equipment import create_equipment_id
 from CSVoperations.create_all_models import create_all_models
-from CSVoperations.find_equipment import find_quipment_id_gym
-from CSVoperations.find_gym import find_gym_id
+from CSVoperations.find_equipment import find_equipment_id_gym, find_one_equipment
+from CSVoperations.find_gym import find_gym_id, find_gym_id_company
 from CSVoperations.create_company import new_company_salida
 from CSVoperations.find_company import find_company_id
-
+from CSVoperations.delete_equipment import delete_equipment
+from CSVoperations.delete_gym import delete_gym
+from CSVoperations.delete_company import delete_company
 app = FastAPI(lifespan=create_all_models)
 @app.get("/")
 async def root():
@@ -22,9 +24,16 @@ async def get_company(id: int):
 async def gimnasio_get(id: int):
     return find_gym_id(id)
 
-@app.get(f"/equipment_of_gym/{id}")
+@app.get(f"/equipment/{id}")
+async def equipment_get(id: int):
+    return find_one_equipment(id)
+
+@app.get(f"/gyms-of-company/{id}")
+async def get_gyms_of_company(id: int):
+    return find_gym_id_company(id)
+@app.get(f"/equipment-of-gym/{id}")
 async def get_equipment_of_gym(id: int):
-    return find_quipment_id_gym(id)
+    return find_equipment_id_gym(id)
 
 @app.post('/company', response_model=CompanySalida)
 async def company_post(company: CompanyEntrada):
@@ -40,3 +49,15 @@ async def equipment_post(equipment: Equipment):
     if "Error" in find_gym_id(equipment.id_gym):
         return {"Error": "Gym no existe"}
     return create_equipment_id(equipment)
+
+@app.delete(f"/equipment/{id}")
+async def equipment_delete(id: int):
+    return delete_equipment(id)
+
+@app.delete(f"/gimnasio/{id}")
+async def gimnasio_delete(id: int):
+    return delete_gym(id)
+
+@app.delete(f"/company/{id}")
+async def company_delete(id: int):
+    return delete_company(id)
