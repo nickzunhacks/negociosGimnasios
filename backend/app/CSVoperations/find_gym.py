a955e4ee82d4bd7870988b714b3d23e2384d0970
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-04-27 20:59:40 -0500

    [ADD] se agrega funcion para encontrar todos los gimnasios de una compania

--- backend/app/CSVoperations/find_gym.py
+++ backend/app/CSVoperations/find_gym.py
@@ -1,11 +1,24 @@
 import pandas as pd
+from CSVoperations.find_company import find_company_id
 
 def find_gym_id(id: int):
 
     dataFrame = pd.read_csv('gimnasios.csv')
-    filtrado = dataFrame[dataFrame['id_gimnasio'] == id]
+    filtrado = dataFrame[ (dataFrame['id_gimnasio'] == id) & (dataFrame['activo'] == True) ]
 
     if filtrado.empty:
         return {"Error":"No Existe este gimnasio"}
     else:
         return filtrado.iloc[0].to_dict()
+
+def find_gym_id_company(id: int):
+    dataFrame = pd.read_csv('gimnasios.csv')
+    company = find_company_id(id)
+
+    if "Error" in company:
+        return {"Error":"company no encontrada"}
+
+    company_id = company['id_company']
+
+    gimnasios = dataFrame[dataFrame['id_company'] == company_id]
+    return gimnasios.to_dict('records')
