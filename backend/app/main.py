d1776b59a209e8fc53c659c068f31d63b4118cde
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-04-29 09:24:02 -0500

    [ADD] se agrega endpoints patch y del nuevo find

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI
 from models.equipment import EquipmentId,Equipment
-from models.gimnasio import GimnasioEntrada, GimnasioSalida
+from models.gimnasio import GimnasioEntrada, GimnasioSalida, TiposGimnasios
 from models.company import CompanySalida, CompanyEntrada
 from CSVoperations.create_gym import create_gym
 from CSVoperations.create_equipment import create_equipment_id
 from CSVoperations.create_all_models import create_all_models
 from CSVoperations.find_equipment import find_equipment_id_gym, find_one_equipment
-from CSVoperations.find_gym import find_gym_id, find_gym_id_company
+from CSVoperations.find_gym import find_gym_id, find_gym_id_company, find_gyms_category
 from CSVoperations.create_company import new_company_salida
 from CSVoperations.find_company import find_company_id
 from CSVoperations.delete_equipment import delete_equipment
 from CSVoperations.delete_gym import delete_gym
 from CSVoperations.delete_company import delete_company
+from CSVoperations.mod_company import edit_name
+from CSVoperations.mod_gym import edit_name_gym
+from CSVoperations.mod_equipment import edit_description_equipment
 app = FastAPI(lifespan=create_all_models)
 @app.get("/")
 async def root():
@@ -35,6 +38,10 @@ async def get_gyms_of_company(id: int):
 async def get_equipment_of_gym(id: int):
     return find_equipment_id_gym(id)
 
+@app.get(f'/gym-category/{type}')
+async def get_gym_category(type: TiposGimnasios):
+    return find_gyms_category(type)
+
 @app.post('/company', response_model=CompanySalida)
 async def company_post(company: CompanyEntrada):
     return new_company_salida(company)
@@ -50,6 +57,17 @@ async def equipment_post(equipment: Equipment):
         return {"Error": "Gym no existe"}
     return create_equipment_id(equipment)
 
+@app.patch("/name-company/{name}/{id}")
+def edit_name_company(name: str, id: int):
+    return edit_name(name, id)
+
+@app.patch("/name-gym/{name}/{id}")
+def edit_name_company(name: str, id: int):
+    return edit_name_gym(name, id)
+
+@app.patch("/description-equipment/{description}/{id}")
+def edit_name_company(description: str, id: int):
+    return edit_description_equipment(description, id)
 @app.delete(f"/equipment/{id}")
 async def equipment_delete(id: int):
     return delete_equipment(id)
