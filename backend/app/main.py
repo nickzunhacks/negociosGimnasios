41bdb8108d69febe0e2ca1534d2b0450877d4205
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-06-02 13:25:49 -0500

    [UPDATE] endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,29 +1,29 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from starlette.templating import Jinja2Templates
-from pathlib import Path
+from fastapi import FastAPI, Request, UploadFile, HTTPException, Form, File
+from sqlmodel import Session
+import json
+from db.db import get_session
+from fastapi.params import Depends
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
 from db.db import Conversation, create_all_tables
 from db.operations_db import create_location, create_company, show_companies, show_locations, show_locations_all
 from models.location import  Location
 from models.company import Company
+from middlewares.verificar_img import is_image
 from models.equipment import Equipment
+from pathlib import Path
 
-#Eliminar proceso de consola trabada
-
-#C:\Users\sala1.FI-LABSISTEMAS.001>tasklist | findstr python
-#python.exe                   11552 Console                    2    79.584 KB
-
-#C:\Users\sala1.FI-LABSISTEMAS.001>taskkill /PID 11552 /F
-#Correcto: se terminó el proceso con PID 11552.
-
-BASE_DIR = Path(__file__).resolve().parent
+BASE = Path(__file__).resolve().parent
 
 app = FastAPI(lifespan=create_all_tables)
-templates = Jinja2Templates(directory= BASE_DIR.parent.parent / "frontend" / "templates")
+templates = Jinja2Templates(directory=BASE.parent.parent / "frontend" / "templates")
+app.mount("/static", StaticFiles(directory=BASE.parent.parent / "frontend" / "static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
-def root(request: Request):
-    return templates.TemplateResponse(request,"home.html")
+@app.get("/search-gym", response_class=HTMLResponse)
+async def search_gym(request: Request, session: Conversation):
+    locations = show_locations_all(session)
+    return templates.TemplateResponse(request, "buscador_gym.html", {"locations":[loc.model_dump() for loc in locations]})
 
 @app.get("/companies")
 def get_companies(session: Conversation):
@@ -42,5 +42,9 @@ async def post_company(company: Company, session: Conversation):
     return create_company(company, session)
 
 @app.post("/location")
-async def post_location(location: Location, session: Conversation):
-    return create_location(location, session)
+async def post_location(location: str = Form(...), img: UploadFile = File(...), session: Session = Depends(get_session)):
+    location_object = Location(**json.loads(location))
+    image = is_image(img)
+    if not(image):
+        raise HTTPException(status_code=415, detail="Not an image")     
+    return await create_location(location_object, img, session)
