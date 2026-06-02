782d37211e28f3da4c3096bdf5889f65256c88aa
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-06-02 13:25:41 -0500

    [UPDATE] inyecta la lon y lat en la creacion de los modelos

--- backend/app/db/operations_db.py
+++ backend/app/db/operations_db.py
@@ -1,6 +1,9 @@
 from sqlmodel import Session, select
+from fastapi import UploadFile
 from models.location import Location, LocationId
 from models.company import Company, CompanyId
+from middlewares.geolocation_converter import coordenadas
+from middlewares.suprabase import save_img
 
 def create_company(company: Company, session: Session):
     new_company = CompanyId.model_validate(company)
@@ -9,8 +12,17 @@ def create_company(company: Company, session: Session):
     session.refresh(new_company)
     return new_company
 
-def create_location(location: Location, session: Session):
+async def create_location(location: Location, img: UploadFile, session: Session):
     new_location = LocationId.model_validate(location)
+    print(f"Buscando: {new_location.address}...")
+    coord = await coordenadas(location.address)
+    print(f"resultado: {coord}")
+    print("guardando imagen...")
+    imagen = save_img(img)
+    print("imagen guardada!: ", imagen)
+    new_location.latitude = coord[0]
+    new_location.longitude = coord[1]
+    new_location.photo_url = imagen
     session.add(new_location)
     session.commit()
     session.refresh(new_location)
