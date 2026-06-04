440611ec9fadfe5df829b3d2f973512fb812f709
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-06-04 01:06:20 -0500

    [ADD] Se agrega operaciones necesarias

--- backend/app/db/operations_db.py
+++ backend/app/db/operations_db.py
@@ -3,10 +3,11 @@ from sqlmodel import Session, select, or_, and_
 from models.gymTypes import GymTypes
 from models.location import Location, LocationId
 from models.company import Company, CompanyId
+from models.equipment import EquipmentId, Equipment
 from middlewares.geolocation_converter import coordenadas
 from middlewares.suprabase import save_img
 
-def create_company(company: Company, session: Session):
+async def create_company(company: Company, session: Session):
     new_company = CompanyId.model_validate(company)
     session.add(new_company)
     session.commit()
@@ -19,6 +20,21 @@ async def create_location(location: Location, session: Session):
     session.commit()
     session.refresh(new_location)
     return new_location
+
+async def create_equipment(equipment: Equipment, session: Session):
+
+    print(equipment)
+
+    location = find_one_location(session, equipment.id_location)
+
+    if location is None:
+        return None
+
+    new_equipment = EquipmentId.model_validate(equipment)
+    session.add(new_equipment)
+    session.commit()
+    session.refresh(new_equipment)
+    return new_equipment
 
 def show_companies(session: Session):
     companies = session.exec(select(CompanyId)).all()
@@ -44,3 +60,20 @@ def show_locations_filtered(name: str, category: GymTypes, session: Session):
             and_(LocationId.name == name, LocationId.type_gym == category)
         )
     ).all()
+
+def show_equipment_gym(session: Session, id: int):
+
+    location = find_one_location(session, id)
+
+    if location is None:
+        return None
+
+    return session.exec( select(EquipmentId).where(EquipmentId.id_location == id) ).all()
+
+def find_one_location(session: Session, id_location: int):
+    location = session.exec(select(LocationId).where(LocationId.id_location == id_location)).one_or_none()
+
+    if location is None:
+        return None
+
+    return location
