006f4d3bace9039772867ffbd5dc09ee512611f4
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-06-05 01:09:54 -0500

    [ADD] se cambia agrega funcion para mostrar error.html dependiendo el caso

---
+++ backend/app/middlewares/error.py
@@ -0,0 +1,10 @@
+from fastapi import Request
+
+def error_handler(request: Request, templates, error_code: str, error_msg: str, img_name: str, volver_url: str, reintentar_url: str):
+    return templates.TemplateResponse(request, "error.html", {
+        "error_code": error_code,
+        "error_msg": error_msg,
+        "imagen": img_name,
+        "volver": volver_url,
+        "reintentar": reintentar_url
+    })
