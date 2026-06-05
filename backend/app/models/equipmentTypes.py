692e0a26c79c1ec60c6114f08573770639dd9264
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-06-05 01:09:37 -0500

    [ADD] se cambia destino estatico a dinamico

--- backend/app/models/equipmentTypes.py
+++ backend/app/models/equipmentTypes.py
@@ -3,11 +3,11 @@ from enum import Enum
 
 class EquipmentTypes(str, Enum):
 
-    CARDIOVASCULAR_MACHINE = "cardiovascular machine"
-    STRENGTH_MACHINE = "strength machine"
-    DUMBBELLS = "dumbbells"
-    BENCH = "bench"
-    ELASTIC_BAND ="elastic band"
-    OTHER = "other"
+        CARDIOVASCULAR_MACHINE = "cardiovascular machine"
+        STRENGTH_MACHINE = "strength machine"
+        DUMBBELLS = "dumbbells"
+        BENCH = "bench"
+        ELASTIC_BAND ="elastic band"
+        OTHER = "other"
 
 
