8c6f34f57bdfdfa430eda6877c05462f2103532a
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-06-05 01:08:27 -0500

    [ADD] se agrega relacion con company

--- backend/app/models/company.py
+++ backend/app/models/company.py
@@ -1,12 +1,16 @@
 from sqlmodel import SQLModel, Field
 
 class Company(SQLModel):
-    id_owner: int = Field(...)
-    name: str = Field(...)
-    logoUrl: str = Field(...)
-    phone: str = Field(...)
-    email: str = Field(...)
-    multiLocation: bool = Field(...)
+        id_owner: int = Field(foreign_key = "ownerid.id_owner")
+        name: str = Field(...)
+        logoUrl: str = Field(...)
+        phone: str = Field(...)
+        email: str = Field(...)
 
 class CompanyId(Company, table=True):
     id_company: int | None = Field(default=None, primary_key=True)
+
+class CompanyUpdate(SQLModel):
+        name: str | None = None
+        phone: str | None = None
+        email: str | None = None
