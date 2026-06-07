5294ddbcb0c9c17f16d5b9513fd8605f7d0d075f
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-06-07 00:17:54 -0500

    [UPDATE] agregamos que si sale la ventana de error, que el navbar corresponda segun si se inicio o no sesion

--- backend/app/middlewares/error.py
+++ backend/app/middlewares/error.py
@@ -1,10 +1,11 @@
 from fastapi import Request
 
-def error_handler(request: Request, templates, error_code: str, error_msg: str, img_name: str, volver_url: str, reintentar_url: str):
+def error_handler(request: Request, templates, error_code: str, error_msg: str, img_name: str, volver_url: str, reintentar_url: str, mod: str):
     return templates.TemplateResponse(request, "error.html", {
         "error_code": error_code,
         "error_msg": error_msg,
         "imagen": img_name,
         "volver": volver_url,
-        "reintentar": reintentar_url
+        "reintentar": reintentar_url,
+        "mod": mod
     })
