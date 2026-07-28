aa018f914b32bbc2817140cf90ebb8ee6665dfa2
Nicolas Rios <nicolas.rioscon@gmail.com>
2026-07-27 23:11:28 -0500

    [UPDATE] corregimos la ruta por los cambios de la ruta al organizar codigo

--- backend/app/middlewares/error.py
+++ backend/app/middlewares/error.py
@@ -1,7 +1,7 @@
 from fastapi import Request
 
 def error_handler(request: Request, templates, error_code: str, error_msg: str, img_name: str, volver_url: str, reintentar_url: str, mod: str):
-    return templates.TemplateResponse(request, "error.html", {
+    return templates.TemplateResponse(request, "partials/error.html", {
         "error_code": error_code,
         "error_msg": error_msg,
         "imagen": img_name,
